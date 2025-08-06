77e5760103a1d307b11ad07035c5b101cbc4b4e1
yuvi3120456 <163744425+yuvi3120456@users.noreply.github.com>
2025-08-06 10:02:07 +0530

    Update Aboutus.jsx

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -5,7 +5,7 @@ import { Link, Outlet } from "react-router-dom";
 const AboutUs = () => {
   return (
     <div className="about-container">
-      <h2>About Our Electronic Store</h2>
+      <h2>Yuvaraju</h2>
       <p>
         Welcome to our E-Commerce platform, your one-stop shop for the latest and greatest in electronics!
         We specialize in high-quality products, including computers, mobiles, laptops, and accessories.
